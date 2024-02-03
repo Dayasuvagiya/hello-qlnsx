3c030c39a376ad36f7df52ad99b9d8d8e4762735
Diya Suvagiya <diya.jivani.diji@gmail.com>
2024-02-03 16:58:57 +0100

    Create basi Form and handle functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import LoginPage from './components/LoginPage'
 
 function App() {
 
   return (
     <>
-      <div>Hello World</div>
+      <LoginPage />
     </>
   )
 }
--- src/components/loginPage.jsx
+++ src/components/loginPage.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import { useState } from 'react'
 
-const loginPage = () => {
+const LoginPage = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+const handleEmail = (event) => {
+  setEmail(event.target.value)
+}
+
+const handlePassword = (event) => {
+    setPassword(event.target.value)
+}
+
   return (
     <div>
-      
+      <h1>QlsnX</h1>
+      <label>Email:</label>
+      <input value={email} onChange={handleEmail} type='text'></input>
+      <label>Password:</label>
+      <input value={email} onChange={handleEmail} type='text'></input>
+      <div>
+        <button>Continue</button>
+      </div>
     </div>
   )
 }
 
-export default loginPage
+export default LoginPage

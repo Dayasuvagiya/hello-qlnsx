f80896d63965d06c097c848c20ef75effbaf848c
Diya Suvagiya <diya.jivani.diji@gmail.com>
2024-02-04 19:25:47 +0100

    Create page 1

--- src/components/loginPage.jsx
+++ src/components/loginPage.jsx
@@ -1,27 +1,36 @@
 import React from 'react'
 import { useState } from 'react'
+import axios from 'axios'
 
 const LoginPage = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-
+console.log({email, password})
 const handleEmail = (event) => {
   setEmail(event.target.value)
 }
 
-const handlePassword = (event) => {
-    setPassword(event.target.value)
+const handleApi = () => {
+  axios.post('http://api.qinsx.de/user/', { 
+    username: email,
+    password: 'xyz',
+  })
+    .then(result => {
+        console.log(result.data)
+        alert('Sucessfully done')
+    })
+    .catch(err => {
+        console.log(err)
+        alert('Server error')
+    })
 }
 
   return (
     <div>
       <h1>QlsnX</h1>
-      <label>Email:</label>
-      <input value={email} onChange={handleEmail} type='text'></input>
-      <label>Password:</label>
-      <input value={email} onChange={handleEmail} type='text'></input>
+      Username: <input value={email} onChange={handleEmail} type='text'></input>
       <div>
-        <button>Continue</button>
+        <button onClick={handleApi}>Continue</button>
       </div>
     </div>
   )
